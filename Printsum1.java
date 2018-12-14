ded88b3fcf76349f65d2e9b628641ba8e78140f9
shyam <shyamarjun@gmail.com>
2018-12-14 16:27:36 +0530

    added

---
+++ Printsum1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Printsum1{
+public static void main(String args[]){  
+
+int n=10,i;
+float Sum=0;
+
+for(i=1;i<n;i++)
+{
+Sum=Sum+(float)1/power(2,0);
+}
+System.out.println("nSum="+Sum);
+
+
+
+
+
+
+   }
+}  
